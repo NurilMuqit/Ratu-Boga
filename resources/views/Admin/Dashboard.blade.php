033e0620746c6da026c1b1ea18e32b0aa8560ca8
muslih2018 <sahmatmuslih@gmail.com>
2023-11-01 08:31:41 +0700

    feat: :sparkles: Enhance admin dashboard view, add admin middleware, and introduce new routes for admin dashboard navigation menu

--- resources/views/Admin/Dashboard.blade.php
+++ resources/views/Admin/Dashboard.blade.php
@@ -1,49 +1,49 @@
+<x-HtmlPage>
+    <div class="p-2 w-screen h-screen ">
+        <div class="h-full w-full">
+            <div class="h-full overflow-hidden  shadow-xl sm:rounded-lg">
+                <!-- resources/views/components/AdminLayout.blade.php -->
+                <div class="flex h-full w-full">
+                    <div class="w-2/12 h-full ">
+                        <x-AdminNavbar />
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin-Dashboard</title>
-</head>
-<body>
-    <div class="container min-h-screen ">
-  
-      <div class="sidebar mt-5 bg-teal-800 w-72 h-96 rounded-lg">
-  
-        <div class="profile d-flex justify-content-center">
-            <div class="list-item">
-              <img class="adm-profile" src="assets/images/Profile.jpg" alt="">
+                    </div>
+
+                    <div class="w-10/12 px-8 ml-2 bg-white rounded-lg text-junggleGreen">
+                        <header class=" py-10  place-items-center grid grid-cols-2  rounded-lg">
+                            {{-- header-title --}}
+                            <div class="h-full w-full ">
+                                <h1 class="font-PlayfairDisplay  text-4xl text-gray-900">
+                                    Dashboard
+                                </h1>
+                                <p>Hi, {{ explode(' ', Auth::user()->name)[0] }} Welcome back to Ratu Boga Admin!</p>
+
+                            </div>
+
+                            <div class="flex justify-end items-center h-full w-full ">
+                                <img src="{{ asset('images/logo.png') }}" alt="" class="w-10 ">
+                                <span
+                                    class="self-center ml-2 PlayfairDisplay text-xl font-semibold whitespace-nowrap text-jungglegreen font-PlayfairDisplay">Ratu
+                                    Boga</span>
+                            </div>
+
+
+                        </header>
+                        <main>
+                            {{-- head main --}}
+                            <div class="grid items-center  grid-cols-2 ">
+                                <h3 class="font-bold text-flame">Sales Summary</h3>
+                                <div class="flex justify-end">
+                                    <img src="{{ asset('images/logo.png') }}" alt="" class="w-10">
+                                </div>
+                            </div>
+
+                        </main>
+
+                    </div>
+                </div>
+
             </div>
         </div>
-  
-        <div class="name ">
-          <div class="list-item">           
-              <x-app-layout>
-            </x-app-layout>
-          </div>
-        </div>
-  
-        <div class="position m-2">
-          <div class="list-item">           
-              <span class="admin-position flex justify-center text-white text-base font-normal" id="admin_position" >Admin Restaurant</span>
-          </div>
-        </div>
-          <ul class="list ">
-            <li class=""><a href="#" class="text-decoration-none"><i class="bi bi-bar-chart-fill"></i>Dashboard</a></li>
-            <li class=""><a href="#" class="text-decoration-none"><i class="bi bi-cart2"></i>Poducts</a></li>
-            <li class=""><a href="#" class="text-decoration-none"><i class="bi bi-bag"></i>Orders</a><span class="notification rounded-pill text-light py-0 px-1">02</span></li>
-            <li class=""><a href="#" class="text-decoration-none"><i class="bi bi-person-lines-fill"></i>Customers</a></li>
-            <li class=""><a href="#" class="text-decoration-none"><i class="bi bi-credit-card"></i>Finance</a></li>
-            <li class=""><a href="#" class="text-decoration-none"><i class="bi bi-gear"></i>Settings</a></li>
-          </ul>
-      </div>
-  
-      <div class="main-content">
-        
-      </div>
     </div>
-  </body>
-</html>
-
+</x-HtmlPage>
---
+++ resources/views/components/html-page.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Ratu-Boga') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="icon" href="{{ URL('images/favicon.ico') }}" type="image/x-icon">
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite('resources/css/app.css')
+    {{-- Menyembunyikan Scrollbar --}}
+    <style>
+        .hide-scroll-y::-webkit-scrollbar {
+            width: 0rem;
+        }
+
+        .hide-scroll-y::-webkit-scrollbar-thumb {
+            background-color: transparent;
+        }
+    </style>
+    <!-- Styles -->
+    @livewireStyles
+</head>
+
+<body class="bg-almond m-0 p-0 hide-scroll-y overflow-y-scroll font-PlusJakartaSans antialiased">
+    {{-- <x-banner /> --}}
+
+
+    <div class="min-h-screen bg-gray-100 ">
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+
+    @stack('modals')
+
+    @livewireScripts
+</body>
+
+</html>
