2f32c1262cf036f84e31203270b703c46539ef1d
Muhammad Nuril Muqit <nurilmuqit@gmail.com>
2023-11-30 03:47:22 +0800

    build: :art: Create Cart Table

---
+++ database/migrations/2023_11_29_190519_create_carts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\NullableType;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->timestamp('order_date')->nullable(false);
+            $table->unsignedBigInteger('total_price')->nullable(false);
+            $table->string('status')->nullable(false);
+            $table->string('payment_method')->nullable(false);
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
